98d4b772fdab324351f7ad2f2076a32e78f57b64
Alexandr <onepunch.zone@yandex.ru>
2025-02-02 04:39:02 +0500

    добавил модуль spring-data-jdbc

---
+++ spring-data-jdbc/src/test/java/ru/otus/java/pro/spring/data/jdbc/spring_data_jdbc/SpringDataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.java.pro.spring.data.jdbc.spring_data_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
